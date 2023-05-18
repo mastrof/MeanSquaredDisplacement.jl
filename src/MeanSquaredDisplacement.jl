6a6408a707c247a3636259dab618a6321b8cbcbc
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-05-18 23:56:16 +0200

    add ensemble msd for uneven trajectories

--- src/MeanSquaredDisplacement.jl
+++ src/MeanSquaredDisplacement.jl
@@ -10,6 +10,32 @@ export emsd, imsd, unfold!
 
 #== Ensemble MSD ==#
 """
+    emsd(x::AbstractVector, t₀::AbstractVector)
+Return the ensemble average of the mean squared displacement of each vector in `x`.
+The timeseries in `x` can have different lengths, and different starting times `t₀`.
+"""
+function emsd(x::AbstractVector{T}, t₀::AbstractVector{<:Integer}) where {T<:AbstractVector}
+    @assert length(x) == length(t₀)
+    individual_msds = imsd.(x)
+    indices = [t₀[i] .+ eachindex(individual_msds[i]) .- 1 for i in eachindex(t₀)]
+    t₁, t₂ = extrema(vcat(indices...))
+    S = eltype(first(individual_msds))
+    out = AxisArray(zeros(S, t₂-t₁+1), t=t₁:t₂)
+    counter = AxisArray(zeros(Int, t₂-t₁+1), t₁:t₂)
+    for i in eachindex(t₀)
+        for j in eachindex(x[i])
+            t = t₀[i]+j-1
+            out[atvalue(t)] += individual_msds[i][j]
+            counter[atvalue(t)] += 1
+        end
+    end
+    for t in t₁:t₂
+        out[atvalue(t)] /= counter[atvalue(t)]
+    end
+    return out
+end
+
+"""
     emsd(x::AbstractMatrix, [lags])
 Return the ensemble average of the mean squared displacement of each column
 of `x` at lag times `lags`.
