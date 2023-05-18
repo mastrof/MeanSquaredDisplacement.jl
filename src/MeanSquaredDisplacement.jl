593c7775e2fc175ecdfd17ed3260e747d9517085
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-05-18 22:51:59 +0200

    rename acf functions and move them to separate file

--- src/MeanSquaredDisplacement.jl
+++ src/MeanSquaredDisplacement.jl
@@ -25,7 +25,7 @@ If not specified `lags` defaults to `0:length(x)-1`.
 """
 function imsd(x::AbstractVector, lags::AbstractVector{<:Integer}=0:size(x,1)-1)
     l = length(x)
-    S₂ = smartacf(x, lags)
+    S₂ = acf(x, lags)
     D = OffsetArray([0.0; dot.(x,x); 0.0], -1:l)
     Q = 2*sum(D)
     S₁ = AxisArray(similar(S₂), lags)
@@ -38,85 +38,8 @@ function imsd(x::AbstractVector, lags::AbstractVector{<:Integer}=0:size(x,1)-1)
     @. S₁ - 2S₂
 end
 
-"""
-    smartacf(x, lags)
-Return the autocovariance function of timeseries `x` at lag times `lags`, using
-a raw acf calculation when the timeseries is small (less than 1024 samples), or
-a FFT-based calculation when the timeseries is large.
-"""
-@inline smartacf(x, lags) = size(x,1) < 1024 ? acf(x, lags) : fftacf(x, lags)
 
-#== Autocovariance function with FFT ==#
-function fftacf(x::AbstractVector{<:Number}, lags=0:size(x,1)-1)
-    S = float(eltype(x))
-    out = Vector{S}(undef, length(lags))
-    fftacf!(out, x, lags)
-end
-function fftacf!(r::AbstractVector, x::AbstractVector, lags)
-    lx = length(x)
-    m = length(lags)
-    @assert length(r) == m
-    StatsBase.check_lags(lx, lags)
-    A = conv(x, reverse(x))
-    for k in 1:m
-        δ = lags[k]
-        r[k] = A[δ+lx]/(lx-δ)
-    end
-    return r
-end
-function fftacf(x::AbstractVector{T}, lags=0:size(x,1)-1) where {T<:Union{AbstractVector,NTuple}}
-    S = float(eltype(eltype(x)))
-    out = Vector{S}(undef, length(lags))
-    fftacf!(out, x, lags)
-end
-function fftacf!(r::AbstractVector, x::AbstractVector{T}, lags) where {T<:Union{AbstractVector,NTuple}}
-    lx = length(x)
-    m = length(lags)
-    @assert length(r) == m
-    StatsBase.check_lags(lx, lags)
-    y = [getindex.(x,i) for i in eachindex(first(x))]
-    A = sum([conv(s, reverse(s)) for s in y])
-    for k in 1:m
-        δ = lags[k]
-        r[k] = A[δ+lx]/(lx-δ)
-    end
-    return r
-end
-
-#== Autocovariance function ==#
-# We follow the structure of `StatsBase.autocov` but
-# 1. allow for non-scalar timeseries
-# 2. modify the normalization
-"""
-    acf(x, [lags])
-Return the autocovariance function of timeseries `x` at lag times `lags`.
-"""
-function acf(x::AbstractVector{<:Number}, lags=0:size(x,1)-1)
-    S = float(eltype(x))
-    out = Vector{S}(undef, length(lags))
-    acf!(out, x, lags)
-end
-function acf(x::AbstractVector{T}, lags=0:size(x,1)-1) where {T<:Union{AbstractVector,NTuple}}
-    S = float(eltype(eltype(x)))
-    out = Vector{S}(undef, length(lags))
-    acf!(out, x, lags)
-end
-
-function acf!(r::AbstractVector, x::AbstractVector, lags)
-    lx = length(x)
-    m = length(lags)
-    @assert length(r) == m
-    StatsBase.check_lags(lx, lags)
-    for k in 1:m
-        δ = lags[k]
-        r[k] = StatsBase._autodot(x, lx, δ) / (lx-δ)
-    end
-    return r
-end
-
-# Extend to accept non-scalar timeseries
-StatsBase._autodot(x::AbstractVector{T}, lx::Int, l::Int) where {T<:Union{AbstractVector,NTuple}} =
-    dot(view(x, 1:(lx-l)), view(x, (1+l):lx))
+include("acf.jl")
 
 
 #== Unfolding ==#
---
+++ src/acf.jl
@@ -0,0 +1,79 @@
+"""
+    acf(x, lags)
+Return the autocovariance function of timeseries `x` at lag times `lags`, using
+a raw acf calculation when the timeseries is small (less than 1024 samples), or
+a FFT-based calculation when the timeseries is large.
+"""
+@inline acf(x, lags) = size(x,1) < 1024 ? rawacf(x, lags) : fftacf(x, lags)
+
+#== Autocovariance function with FFT ==#
+function fftacf(x::AbstractVector{<:Number}, lags=0:size(x,1)-1)
+    S = float(eltype(x))
+    out = Vector{S}(undef, length(lags))
+    fftacf!(out, x, lags)
+end
+function fftacf!(r::AbstractVector, x::AbstractVector, lags)
+    lx = length(x)
+    m = length(lags)
+    @assert length(r) == m
+    StatsBase.check_lags(lx, lags)
+    A = conv(x, reverse(x))
+    for k in 1:m
+        δ = lags[k]
+        r[k] = A[δ+lx]/(lx-δ)
+    end
+    return r
+end
+
+function fftacf(x::AbstractVector{T}, lags=0:size(x,1)-1) where {T<:Union{AbstractVector,NTuple}}
+    S = float(eltype(eltype(x)))
+    out = Vector{S}(undef, length(lags))
+    fftacf!(out, x, lags)
+end
+function fftacf!(r::AbstractVector, x::AbstractVector{T}, lags) where {T<:Union{AbstractVector,NTuple}}
+    lx = length(x)
+    m = length(lags)
+    @assert length(r) == m
+    StatsBase.check_lags(lx, lags)
+    y = [getindex.(x,i) for i in eachindex(first(x))]
+    A = sum([conv(s, reverse(s)) for s in y])
+    for k in 1:m
+        δ = lags[k]
+        r[k] = A[δ+lx]/(lx-δ)
+    end
+    return r
+end
+
+#== Autocovariance function with autodot ==#
+# We follow the structure of `StatsBase.autocov` but
+# 1. allow for non-scalar timeseries
+# 2. modify the normalization
+function rawacf(x::AbstractVector{<:Number}, lags=0:size(x,1)-1)
+    S = float(eltype(x))
+    out = Vector{S}(undef, length(lags))
+    rawacf!(out, x, lags)
+end
+function rawacf(x::AbstractVector{T}, lags=0:size(x,1)-1) where {T<:Union{AbstractVector,NTuple}}
+    S = float(eltype(eltype(x)))
+    out = Vector{S}(undef, length(lags))
+    rawacf!(out, x, lags)
+end
+
+function rawacf!(r::AbstractVector, x::AbstractVector, lags)
+    lx = length(x)
+    m = length(lags)
+    @assert length(r) == m
+    StatsBase.check_lags(lx, lags)
+    for k in 1:m
+        δ = lags[k]
+        r[k] = StatsBase._autodot(x, lx, δ) / (lx-δ)
+    end
+    return r
+end
+
+# Extend to accept non-scalar timeseries
+StatsBase._autodot(x::AbstractVector{T}, lx::Int, l::Int) where {T<:Union{AbstractVector,NTuple}} =
+    dot(view(x, 1:(lx-l)), view(x, (1+l):lx))
+
+
+
