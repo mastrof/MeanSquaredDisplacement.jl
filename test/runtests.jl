2b698b57ed065bc8496ad0c601455fb980639bca
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-05-19 00:57:27 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using MeanSquaredDisplacement
 using Test
 
 @testset "MeanSquaredDisplacement.jl" begin
+    # ballistic trajectory
     x = collect(1:10)
     m = imsd(x)
     @test length(m) == length(x)
@@ -10,6 +11,10 @@ using Test
     mlags = imsd(x, lags)
     @test mlags ≈ m[lags.+1]
 
+    x = collect(1:10_000)
+    @test imsd(x) ≈ (eachindex(x) .- 1).^2
+
+    # non-scalar msd is the sum of each dimension's msd
     x = [(i,0) for i in 1:10]
     m2 = imsd(x)
     @test m2 ≈ m
@@ -18,8 +23,26 @@ using Test
     m3 = imsd(x)
     @test m3 ≈ 2m
 
+    # imsd over matrix is hcat of each column's imsd
     x = cumsum(randn(100,3), dims=1)
     m = imsd(x)
     m2 = hcat([imsd(x[:,i]) for i in axes(x,2)]...)
     @test m ≈ m2
+
+    # emsd is the average of imsds
+    using StatsBase: mean
+    @test emsd(x) ≈ vec(mean(m2, dims=2))
+
+    x = [collect(1:10), collect(1:10)]
+    @test emsd(x) ≈ imsd(x[1])
+
+    na, nb = 10, 5
+    a = randn(na)
+    b = randn(nb)
+    x = [a, b]
+    m = emsd(x)
+    ma = imsd(a)
+    mb = [imsd(b); repeat([0.0], na-nb)]
+    divisor = [repeat([2], nb); repeat([1], na-nb)]
+    @test m ≈ (ma .+ mb) ./ divisor
 end
